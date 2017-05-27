b8146f5d9d0255dbcbd17304ade5d020977fd94e
Ron Guglielmone <ronniedaymusic@gmail.com>
2017-05-26 19:30:29 -0700

    Update evalstate.cpp

--- Assignment_6/evalstate.cpp
+++ Assignment_6/evalstate.cpp
@@ -3,7 +3,6 @@
  * -------------------
  *
  * Name: Ron Guglielmone
- * Section: Marielos Sanson
  *
  * This file implements the EvalState class, which defines a symbol
  * table for keeping track of the value of identifiers.  The public
